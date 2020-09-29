7d27bc5111afba313b4c05387fea51bc4b6f3003
vpanyushenko <vpanyushenko@outlook.com>
2020-09-29 11:10:44 -0700

    Add custom index template screens from svelte

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions")
+const express = require("express")
+const cors = require("cors")
+const { expressServer } = require("./__sapper__/build/server/server")
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const api = express()
+
+api.use(
+  cors({
+    origin: [
+      //add cors here
+    ],
+    preflightContinue: true,
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+)
+
+exports.ssr = functions.https.onRequest(expressServer)
+exports.api = functions.https.onRequest(api)
